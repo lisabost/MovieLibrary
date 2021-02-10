47b6e5c43b6413a26085d8b164d65fb502dbbc56
Lisa Bost <bost.lisam@gmail.com>
2021-02-10 10:59:05 -0600

    added checking for comma in movie title if comma exists wrap title in quotes

--- Movie.cs
+++ Movie.cs
@@ -1,18 +1,36 @@
 using System;
 using System.Collections.Generic;
 
-namespace MovieLibrary {
+namespace MovieLibrary
+{
 
-    class Movie 
+    class Movie
     {
-        private string movieID;
+        // private string movieID;
 
-        public string title {get; set;}
-        public List<string> genre {get; set;}
+        public List<string> genre { get; set; }
 
         //constructor for our movie object
-        public Movie(){        }
-        
+        public Movie() { }
+
+        //when we set the title check to see if there is a comma in the movie title
+        //if there is wrap the title in quotes
+        public string title
+        {
+            get
+            {
+                    return this.title;
+            }
+            set
+            {
+                //if the index of the comma = -1 there is no comma in the string
+                //so we can set the title of our movie to the string entered
+                this.title = title.IndexOf(",") != -1 ? $"\"{title}\"" : title;
+            }
         }
+
+        //check to see if the movie title is unique
+        public isTitleUnique 
+
     }
 }
